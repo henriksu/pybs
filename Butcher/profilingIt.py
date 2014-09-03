2adeb9647a8ce11248ad800f5ba3ac6c07e5a1aa
Henrik Sperre Sundklakk <henrik.sundklakk@gmail.com>
2014-09-04 00:26:47 +0200

    Added simple profiling

---
+++ Butcher/profilingIt.py
@@ -0,0 +1,21 @@
+# This Python file uses the following encoding: utf-8
+import cProfile as profile
+import pstats
+
+from forest import Forest
+from trees.ButcherTrees import ButcherTree
+
+def make_all_trees_up_to_order(n=11):
+    basetree = ButcherTree(Forest())
+    base_forest = Forest([basetree])
+    result = [len(base_forest)]
+    for i in xrange(n):
+        base_forest = base_forest.D()
+        result.append(len(base_forest))
+
+profile.run('make_all_trees_up_to_order()', filename='MinKuleFil')
+p = pstats.Stats('MinKuleFil')
+#p.strip_dirs()
+p.sort_stats('cumtime')
+p.print_stats()
+pass
