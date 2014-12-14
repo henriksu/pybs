251376a7b9de521d9f982fe6870af3d1adb6ad9a
Henrik Sperre Sundklakk <henrik.sundklakk@gmail.com>
2014-12-14 22:19:07 +0100

    Oppdaterte profileIt

--- Butcher/src/profilingIt.py
+++ Butcher/src/profilingIt.py
@@ -1,23 +1,20 @@
-#!/usr/bin/env sage -python #  TODO: Add to all files.
 # This Python file uses the following encoding: utf-8
 import cProfile as profile
 import pstats
-from sage.all import *
 
-from forest import Forest
-from trees.ButcherTrees import ButcherTree
+
+from src.trees import ButcherEmptyTree
+from src.combinations import LinearCombination, differentiate as D
+
 
 def make_all_trees_up_to_order(n=11):
-    basetree = ButcherTree(Forest())
-    base_forest = Forest([basetree])
-    result = [len(base_forest)]
+    theSum = LinearCombination(ButcherEmptyTree())
     for i in xrange(n):
-        base_forest = base_forest.D()
-        result.append(len(base_forest))
+        theSum = D(theSum)
 
 profile.run('make_all_trees_up_to_order()', filename='MinKuleFil')
 p = pstats.Stats('MinKuleFil')
-#p.strip_dirs()
+# p.strip_dirs()
 p.sort_stats('cumtime')
 p.print_stats()
 pass
