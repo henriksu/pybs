0ae7dcf88dde26fd8024edbf101d54fe700e7b0d
Henrik Sperre Sundklakk <henrik.sundklakk@gmail.com>
2015-03-13 13:38:02 +0100

    First running edition of new generator.

--- pybs/unordered_tree/__init__.py
+++ pybs/unordered_tree/__init__.py
@@ -4,5 +4,3 @@
 from pybs.unordered_tree.unordered_tree import UnorderedTree, leaf
 from pybs.unordered_tree.freeTrees import get_free_tree
 from pybs.unordered_tree.trees import tree_generator, trees_of_order
-
-
--- pybs/unordered_tree/trees.py
+++ pybs/unordered_tree/trees.py
@@ -1,6 +1,6 @@
 from enum import Enum  # TODO: Note dependency on enum34 in the installation.
 from operator import __add__ as _add
-from itertools import imap as _imap
+from itertools import imap as _imap, count as _count
 
 from pybs.utils import memoized
 from pybs.unordered_tree import leaf
@@ -9,7 +9,6 @@ treeType = Enum('treeType', 'ordinary binary symmetric')
 tree_pool = dict()
 tree_pool[treeType.ordinary] = dict()  # TODO: Implement
 tree_pool[treeType.binary] = dict()  # TODO: Implement
-tree_pool[treeType.symmetric] = dict()  # NEEDED? TODO: Implement
 
 
 #class TreeType(object):
@@ -23,33 +22,76 @@ class TreeOrder(object):  # TODO: Find an enum type.
         self.order = order
         self.tree_type = tree_type
         self.trees = None
+        self.trees_are_sorted = False
         self.free_trees = None
-        self.tree_generator  # TODO: Needed?
+        #self.tree_generator  # TODO: Needed?
+
+    def __hash__(self):  # It appears immutable.
+        return hash((hash(self.order), hash(self.tree_type)))
+
+    def __eq__(self, other):
+        return self.order == other.order and self.tree_type == other.tree_type
+
+    def trees_of_order(self, sort=False):
+        if self.trees is not None:
+            if not sort:
+                return self.trees
+            else:  # sort=True
+                if self.trees_are_sorted:
+                    return self.trees
+                else:  # sorting required.
+                    self.trees = sorted(self.trees)
+                    self.trees_are_sorted = True
+                    return self.trees
+        else:  # Must generate the trees.
+            if self.order == 1:
+                self.trees = (leaf,)
+                self.trees_are_sorted = True
+                return self.trees
+            else:
+                try:
+                    previous_trees = tree_pool[self.tree_type][self.order-1].trees_of_order()
+                except KeyError:
+                    tree_order = TreeOrder(self.order-1, self.tree_type)
+                    tree_pool[self.tree_type][self.order-1] = tree_order
+                    previous_trees = tree_order.trees_of_order()
+            self.trees = set()
+            for tree in previous_trees:
+                self.trees.update(_graft_leaf(tree))
+            if sort:
+                self.trees = tuple(sorted(self.trees))  # Tuple to protect from mutability.
+                self.trees_are_sorted = True
+            else:  # Freeze the set
+                self.trees = frozenset(self.trees)  # Modest protection from alteration.
+            return self.trees
 
 
-def tree_generator(sort=False):
+def tree_generator(sort=False, tree_type=treeType.ordinary):
+    if tree_type == treeType.ordinary:
+        return ordinary_tree_generator(sort)
+
+
+def ordinary_tree_generator(sort):
     'Yields all trees by increasing order.'
-    oldSet = set([leaf])
-    while True:
-        for tree in oldSet:
-            yield tree
-        newSet = set()
-        for tree in oldSet:
-            newSet.update(_graft_leaf(tree))
-        if sort:
-            oldSet = sorted(newSet)
-        else:
-            oldSet = newSet
+    for order in _count(1):
+        try:
+            tree_order = tree_pool[treeType.ordinary][order]
+            for tree in tree_order.trees_of_order(sort):
+                yield tree
+        except KeyError:  # TreeOrder didn't exist.
+            tree_order = TreeOrder(order, treeType.ordinary)
+            tree_pool[treeType.ordinary][order] = tree_order
+            for tree in tree_order.trees_of_order(sort):
+                yield tree
 
 
 def trees_of_order(order, sort=False, tree_type=treeType.ordinary):
-    oldSet = set([leaf])
-    for _ in range(order-1):
-        oldSet = _graft_leaf_on_set(oldSet)
-    if sort:
-        oldSet = sorted(oldSet)
-    for tree in oldSet:
-        yield tree
+    try:
+        return tree_pool[tree_type][order].trees_of_order(sort)
+    except KeyError:
+        tree_order = TreeOrder(order, treeType.ordinary)
+        tree_pool[treeType.ordinary][order] = tree_order
+        return tree_order.trees_of_order(sort)
 
 
 def _graft_leaf_on_set(oldSet):
